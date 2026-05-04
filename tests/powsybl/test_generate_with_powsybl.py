9eb051e179a309ee1c13f4f198fb3d67bdd65fb1
tren <tengxiang.ren@artelys.com>
2026-05-04 15:15:51 -0400

    updated test_generate_with_powsybl: delete consistency test

--- tests/powsybl/test_generate_with_powsybl.py
+++ tests/powsybl/test_generate_with_powsybl.py
@@ -10,11 +10,6 @@ pytestmark = pytest.mark.skipif(
     reason="pypowsybl is not installed. Install with: pip install gridfm-datakit[powsybl]",
 )
 
-# Path of the directory containing the reference generated data for comparison
-# Reference data are generated using the IEEE14.m network file
-
-ref_data_dir = Path(__file__).parent / "data" / "reference" / "generated_data" / "IEEE14_m" / "raw"
-
 # ---------------------------------------------------------------------------
 # Fixtures
 # ---------------------------------------------------------------------------
@@ -69,82 +64,50 @@ def config_ieee14_cgmes():
         config = yaml.safe_load(f)
     return config
 
-
 # ---------------------------------------------------------------------------
-# 1. Consistency tests
-# ---------------------------------------------------------------------------
-
-class TestConsistency:
-    """
-    The generated power flow data should be identical if the same seed is used for randomization.
-    The tests may fail if the default load flow configuration has been modified.
-    """
-
-    def test_consistency_ieee14(self, config_ieee14_m):
-        """Generate power flow data using config_ieee14_m and compare it with the reference."""
-        import pandas as pd
-        from gridfm_datakit import generate_power_flow_data
-        
-        generate_power_flow_data(config_ieee14_m)
-
-        outputs_dir = Path(__file__).parent.parent.parent/ config_ieee14_m['settings']['data_dir'] / config_ieee14_m['network']['name'] / 'raw'
-        
-        assert pd.read_parquet(outputs_dir/'branch_data.parquet').equals(pd.read_parquet(ref_data_dir/'branch_data.parquet'))
-        assert pd.read_parquet(outputs_dir/'bus_data.parquet').equals(pd.read_parquet(ref_data_dir/'bus_data.parquet'))
-        assert pd.read_parquet(outputs_dir/'gen_data.parquet').equals(pd.read_parquet(ref_data_dir/'gen_data.parquet'))
-        assert pd.read_parquet(outputs_dir/'y_bus_data.parquet').equals(pd.read_parquet(ref_data_dir/'y_bus_data.parquet'))
-
-# ---------------------------------------------------------------------------
-# 2. Format tests
+# 1. Format tests
 # ---------------------------------------------------------------------------
 
 class TestFormats:
     """
     Tests the ability to handle MATPOWER (.m and .mat extensions), PSS/E, XIIDM and CGMES formats.
     """
+    def test_ieee14_m(self, config_ieee14_m):
+        "Test handling of MATPOWER format (.m extension)"
+        from gridfm_datakit import generate_power_flow_data
+
+        config = config_ieee14_m
+        generate_power_flow_data(config)
+        assert True
 
     def test_ieee14_mat(self, config_ieee14_mat):
         """Test handling of MATPOWER format (.mat extension)."""
-        import pandas as pd
         from gridfm_datakit import generate_power_flow_data
         
         config = config_ieee14_mat
-
         generate_power_flow_data(config)
-
-        outputs_dir = Path(__file__).parent.parent.parent/ config['settings']['data_dir'] / config['network']['name'] / 'raw'
-        
-        assert pd.read_parquet(outputs_dir/'branch_data.parquet').equals(pd.read_parquet(ref_data_dir/'branch_data.parquet'))
-        assert pd.read_parquet(outputs_dir/'bus_data.parquet').equals(pd.read_parquet(ref_data_dir/'bus_data.parquet'))
-        assert pd.read_parquet(outputs_dir/'gen_data.parquet').equals(pd.read_parquet(ref_data_dir/'gen_data.parquet'))
-        assert pd.read_parquet(outputs_dir/'y_bus_data.parquet').equals(pd.read_parquet(ref_data_dir/'y_bus_data.parquet'))
+        assert True
 
     def test_ieee14_raw(self, config_ieee14_raw):
         """Test handling of PSS/E format (.raw extension)."""
-        import pandas as pd
         from gridfm_datakit import generate_power_flow_data
         
         config = config_ieee14_raw
-
         generate_power_flow_data(config)        
         assert True
     
     def test_ieee14_xiidm(self, config_ieee14_xiidm):
         """Test handling of XIIDM format (.xiidm extension)."""
-        import pandas as pd
         from gridfm_datakit import generate_power_flow_data
         
         config = config_ieee14_xiidm
-
         generate_power_flow_data(config)
         assert True
     
     def test_ieee14_cgmes(self, config_ieee14_cgmes):
         """Test handling of CGMES format (.zip extension)."""
-        import pandas as pd
         from gridfm_datakit import generate_power_flow_data
         
         config = config_ieee14_cgmes
-
         generate_power_flow_data(config)
         assert True
